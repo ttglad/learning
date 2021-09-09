398d7a0a2281b512d337806e6f6c01c01a8d3a3f
taoyl <492086163@qq.com>
2021-09-09 16:27:02 +0800

    修改登录页面

--- release/js/choose.js
+++ release/js/choose.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", function () {
     document.getElementById("qr").addEventListener("click", function () {
         chrome.runtime.sendMessage({"method": "chooseLogin"}, {}, function (response) {
-            window.location.replace("https://pc.xuexi.cn/points/my-points.html");
+            // window.location.replace("https://pc.xuexi.cn/points/my-points.html");
+            window.location.replace("https://pc.xuexi.cn/points/login.html");
         });
     });
 });
