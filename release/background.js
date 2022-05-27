884726a0268b5e01ab97c950494fb9b43faa199f
taoyl <492086163@qq.com>
2022-05-27 10:00:57 +0800

    修复学习结束后状态无法变更

--- release/background.js
+++ release/background.js
@@ -257,14 +257,14 @@ function stopStudy() {
     chrome.storage.local.get(["studyWindowId"], function (result) {
         logMessage("stopStudy begin, studyWindowId is: " + result.studyWindowId);
         if (result.studyWindowId) {
-            // 重置参数
-            chrome.storage.local.remove(["studyWindowId", "studyTabId"]);
-            chrome.action.setBadgeText({ text: "" });
             // 关闭窗口
             chrome.windows.remove(result.studyWindowId, function () {
                 noticeMessage(chrome.i18n.getMessage("extFinish"));
                 logMessage("stopStudy success.");
             });
+            // 重置参数
+            chrome.storage.local.remove(["studyWindowId", "studyTabId"]);
+            chrome.action.setBadgeText({ text: "" });
         }
     });
 }
